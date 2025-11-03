b44f8a4fcdc6d4e5d454fa5ffda148ba55c64ce2
wingddd <wongtaisin1024@gmail.com>
2025-11-03 15:56:44 +0800

    优化上滑加载

--- src/pages/chart/index.vue
+++ src/pages/chart/index.vue
@@ -2,7 +2,7 @@
  * @Author: wingddd wongtaisin1024@gmail.com
  * @Date: 2025-11-01 10:32:58
  * @LastEditors: wingddd wongtaisin1024@gmail.com
- * @LastEditTime: 2025-11-03 14:51:57
+ * @LastEditTime: 2025-11-03 15:55:17
  * @FilePath: \wanWanApp\src\pages\chart\index.vue
  * @Description:
  *
@@ -10,59 +10,64 @@
 -->
 <template>
   <view class="chart-page">
-    <uni-row class="top">
-      <uni-col :span="8">
-        <text class="text">{{ month.slice(0, 4) }}年</text>
-        <CommonMothPicker v-model="month" @change="onChange">
-          <p class="money" @click="handleDate">
-            {{ month.slice(5, 7) }}
-            <text style="font-size: 28rpx">月</text>
-          </p>
-        </CommonMothPicker>
-      </uni-col>
-      <uni-col :span="8">
-        <text class="text">收入</text>
-        <p class="money">10086</p>
-      </uni-col>
-      <uni-col :span="8">
-        <text class="text">支出</text>
-        <p class="money">{{ moneyTotal }}</p>
-      </uni-col>
-    </uni-row>
+    <scroll-view scroll-y style="height: calc(100vh - 188rpx)" @scrolltolower="loadMore">
+      <uni-row class="top">
+        <uni-col :span="8">
+          <text class="text">{{ month.slice(0, 4) }}年</text>
+          <CommonMothPicker v-model="month" @change="onChange">
+            <p class="money">
+              {{ month.slice(5, 7) }}
+              <text style="font-size: 28rpx">月</text>
+            </p>
+          </CommonMothPicker>
+        </uni-col>
+        <uni-col :span="8">
+          <text class="text">消费笔数</text>
+          <p class="money">{{ params.total }}</p>
+        </uni-col>
+        <uni-col :span="8">
+          <text class="text">支出</text>
+          <p class="money">{{ moneyTotal }}</p>
+        </uni-col>
+      </uni-row>
 
-    <uni-list v-for="group in tableData" :key="group.date">
-      <view class="list-top">
-        <uni-row>
-          <uni-col :span="12">
-            <text>{{ group.date }}</text>
-          </uni-col>
-          <uni-col :span="12" style="text-align: right">
-            <text>支出：{{ group.total }}</text>
-          </uni-col>
-        </uni-row>
-      </view>
+      <uni-list v-for="group in tableData" :key="group.date">
+        <view class="list-top">
+          <uni-row>
+            <uni-col :span="12">
+              <text>{{ group.date }}</text>
+            </uni-col>
+            <uni-col :span="12" style="text-align: right">
+              <text>支出：{{ group.total }}</text>
+            </uni-col>
+          </uni-row>
+        </view>
 
-      <uni-swipe-action ref="swipeActionRef">
-        <!-- 右侧带角标 -->
-        <uni-swipe-action-item
-          v-for="(item, i) in group.list"
-          :key="item.id"
-          :right-options="options"
-          @click="onClick($event, item)"
-          @change="swipeChange($event, i)"
-        >
-          <uni-list-chat
-            :title="item.shop_name"
-            avatar="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-            :note="item.create_date"
+        <uni-swipe-action ref="swipeActionRef">
+          <!-- 右侧带角标 -->
+          <uni-swipe-action-item
+            v-for="(item, i) in group.list"
+            :key="item.id"
+            :right-options="options"
+            @click="onClick($event, item)"
+            @change="swipeChange($event, i)"
           >
-            <view class="chat-custom-right">
-              <text>-{{ item.money }}</text>
-            </view>
-          </uni-list-chat>
-        </uni-swipe-action-item>
-      </uni-swipe-action>
-    </uni-list>
+            <uni-list-chat
+              :title="item.shop_name"
+              avatar="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+              :note="item.create_date"
+            >
+              <view class="chat-custom-right">
+                <text>-{{ item.money }}</text>
+              </view>
+            </uni-list-chat>
+          </uni-swipe-action-item>
+        </uni-swipe-action>
+      </uni-list>
+
+      <!-- 加载提示 -->
+      <uni-load-more :status="status" />
+    </scroll-view>
   </view>
 
   <ExpensesPopup
@@ -98,26 +103,33 @@ const options = reactive([
 const month = ref(`${new Date().getFullYear()}-${new Date().getMonth() + 1}`)
 const tableData = ref<any>([])
 const params = ref({
-  page: 1,
-  pageSize: 10,
   userId: 1,
   startDate: utils.getCurrentMonthRange(month.value).firstDay,
-  endDate: utils.getCurrentMonthRange(month.value).lastDay
+  endDate: utils.getCurrentMonthRange(month.value).lastDay,
+  page: 1,
+  pageSize: 10,
+  total: 0 // 传了后台也不接受，只用作显示消费笔数
 })
+const status = ref('more') // more/loading/noMore
 const moneyTotal = ref(0)
 const expensesPopupRef = ref()
 const expensesParams = ref<any>({})
 const swipeActionRef = ref<any[]>([] as any[])
 
-/**
- * 初始化数据
- */
 const init = async () => {
-  const result: any = await expensesDetailList(params.value)
+  await initList()
+  await initTotal()
+}
 
-  // 重新设计list，需要把每一天的支出都展示出来
+// 初始化数据
+const initList = async () => {
+  status.value = 'loading'
+  const { list, total }: any = await expensesDetailList(params.value)
+  params.value.total = total
+
+  // 重新设计list，需要把每一天的支出都展示出来，[{list:{}, date:2025-01-01, total:0}]
   const grouped = Object.values(
-    result.list.reduce((acc: any, item: any) => {
+    list.reduce((acc: any, item: any) => {
       const date = item.create_date.split(' ')[0] // 只取年月日
       if (!acc[date]) {
         acc[date] = { date, list: [], total: 0 }
@@ -127,9 +139,15 @@ const init = async () => {
       return acc
     }, {})
   )
-  tableData.value = grouped || []
 
-  // 支出总金额
+  tableData.value = [...tableData.value, ...grouped]
+  // 计算总页数
+  const totalPage = Math.ceil(total / params.value.pageSize)
+  status.value = params.value.page >= totalPage ? 'noMore' : 'more'
+}
+
+// 支出总金额
+const initTotal = async () => {
   const { total }: any = await expensesTotal({
     userId: 1,
     startDate: params.value.startDate,
@@ -138,37 +156,39 @@ const init = async () => {
   moneyTotal.value = total || 0
 }
 
-const handleDate = () => {
-  console.log(`时间`)
-}
-
 const onChange = (e: any) => {
   month.value = e // 可设置可不设置，因为组件里使用了双向绑定
   params.value.startDate = utils.getCurrentMonthRange(month.value).firstDay
   params.value.endDate = utils.getCurrentMonthRange(month.value).lastDay
+  tableData.value = []
+  params.value.page = 1
   init()
 }
 
 const onClick = async (e: any, row: any) => {
-  console.log(e, row)
   if (e.content.text === '修改') {
     expensesParams.value = row
     expensesParams.value.expensesName = row.expenses_name
     expensesParams.value.shopName = row.shop_name
     expensesParams.value.paymentName = row.payment_name
     expensesParams.value.createDate = row.create_date
-    // TODO: 需要一个getInfo 要获取到 shopId 和 paymentId
-    // 使用 setTimeout 延迟打开弹窗，避免事件冒泡导致立即关闭
+    // TODO: 需要一个getInfo 要获取到 shopId 和 paymentId，用来显示下拉框的回显
     setTimeout(() => {
       expensesPopupRef.value.open()
-    }, 100)
+    }, 100) // 使用 setTimeout 延迟打开弹窗，避免事件冒泡导致立即关闭
   } else {
-    Promise.all([await expensesDetailDelete(row.id), await init()])
+    Promise.all([await expensesDetailDelete(row.id), await init()]) // 删除
   }
 }
 
 const swipeChange = (e: any, index: number) => {
   console.log(e, index)
+}
+
+const loadMore = () => {
+  if (status.value !== 'more') return
+  params.value.page++
+  initList()
 }
 
 const onSubmit = async (values: any) => {
