7a45375d8d4b6bf4e646e60d5ef4b1c93094772d
wingddd <wongtaisin1024@gmail.com>
2025-11-05 13:51:49 +0800

    个人中心

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -1,60 +1,68 @@
 <!--
  * @Author: wingddd wongtaisin1024@gmail.com
- * @Date: 2025-10-13 16:12:04
+ * @Date: 2025-11-01 10:33:29
  * @LastEditors: wingddd wongtaisin1024@gmail.com
- * @LastEditTime: 2025-10-28 09:20:26
- * @FilePath: \wanWanApp\src\pages\login\index.vue
+ * @LastEditTime: 2025-11-05 13:48:46
+ * @FilePath: \wanWanApp\src\pages\user\index.vue
  * @Description:
  *
  * Copyright (c) 2025 by wongtaisin1024@gmail.com, All Rights Reserved.
 -->
 <template>
-  <view class="login-page">
-    <uni-forms ref="formRef" :model="params" :rules="rules">
-      <uni-forms-item label="用户名" name="user_name" :required="true">
-        <uni-easyinput v-model="params.user_name" placeholder="请输入用户名" />
-      </uni-forms-item>
-      <uni-forms-item label="密码" name="password" :required="true">
-        <uni-easyinput v-model="params.password" type="password" placeholder="请输入密码" />
-      </uni-forms-item>
-      <button type="primary" size="large" @click="onSubmit">提交</button>
-    </uni-forms>
+  <view class="user-page">
+    <view class="user-header">
+      <uni-row :gutter="20">
+        <uni-col :span="12" class="user-info">
+          <image
+            src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+            class="avatar"
+          />
+          <text>1111</text>
+        </uni-col>
+        <uni-col :span="6" :push="6">
+          <uni-tag
+            text="标签"
+            :circle="true"
+            custom-style="background-color: #fff; border-color: #fff; color: #000;"
+          />
+        </uni-col>
+      </uni-row>
+    </view>
+
+    <uni-card :is-shadow="false" class="user-card">
+      <text>这是一个基础卡片示例，内容较少，此示例展示了一个没有任何属性不带阴影的卡片。</text>
+    </uni-card>
   </view>
 </template>
 
-<script lang="ts" setup>
-import { reactive, ref } from 'vue'
-import { request } from '../../services/request'
-
-const params = reactive({
-  user_name: '大帅',
-  password: '123456'
-})
-const formRef = ref()
-const rules = {
-  user_name: { rules: [{ required: true, errorMessage: '用户名不能为空' }] },
-  password: { rules: [{ required: true, errorMessage: '密码不能为空' }] }
-}
-
-const onSubmit = () => {
-  formRef.value
-    .validate()
-    .then(async (res: any) => {
-      const { token }: any = await request('/login', 'POST', res)
-      console.log('创建成功:', token)
-      uni.showToast({ title: '登录成功', icon: 'success' })
-      uni.setStorageSync('token', token)
-      uni.navigateTo({ url: '/pages/expenses/index' })
-    })
-    .catch((err: any) => {
-      console.error('err', err)
-      uni.showToast({ title: `${err[0].errorMessage}` })
-    })
-}
-</script>
+<script lang="ts" setup></script>
 
 <style scoped lang="scss">
-.login-page {
-  padding: 40rpx;
+.user-page {
+  .user-header {
+    padding: 40rpx;
+    background-color: #fede2b;
+    border-radius: 0 0 40rpx 40rpx;
+
+    .user-info {
+      display: flex;
+      align-items: center;
+      padding-bottom: 40rpx;
+
+      text {
+        margin-left: 20rpx;
+      }
+    }
+
+    .avatar {
+      width: 120rpx;
+      height: 120rpx;
+      border-radius: 50%;
+    }
+  }
+
+  .user-card {
+    margin-top: -40rpx !important;
+  }
 }
 </style>
