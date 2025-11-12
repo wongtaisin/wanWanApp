99e767431a1f0c6aae71f18e3d5627fdce31291f
wingddd <wongtaisin1024@gmail.com>
2025-11-12 10:06:18 +0800

    配置jsx，兼容小程序

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,29 @@
  * @Author: wingddd wongtaisin1024@gmail.com
  * @Date: 2025-10-08 15:10:00
  * @LastEditors: wingddd wongtaisin1024@gmail.com
- * @LastEditTime: 2025-10-29 14:46:55
+ * @LastEditTime: 2025-11-12 10:03:04
  * @FilePath: \wanWanApp\vite.config.ts
- * @Description: Vite 配置，统一转换 uni-ui px
+ * @Description: Vite 配置
  */
 
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import postcssPxtransform from 'postcss-pxtransform'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 // import designWidth from './src/utils/designWidth' // TODO: 无法很好的转换，丢弃
 
 const platform = process.env.UNI_PLATFORM
 
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [
+    uni(),
+    vueJsx({
+      // ✅ 以下配置确保在小程序端 JSX 可正常编译
+      transformOn: true, // 支持 onClick / onTap 等事件
+      mergeProps: true, // 合并相同属性（防止重复属性警告）
+      enableObjectSlots: false // 避免部分平台 slot 编译出错
+    })
+  ],
   // css: {
   //   postcss: {
   //     plugins: [
